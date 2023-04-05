71cd2ac649e2b8983c061f12a584b40e6c838d49
MiguelCidadeSilva <mikefsi@hotmail.com>
2023-04-05 15:54:52 +0100

    Criação de template para facilitar o desenvolvimento.

--- WebSite/routes/index.js
+++ WebSite/routes/index.js
@@ -2,11 +2,20 @@ var express = require('express');
 var router = express.Router();
 var Rua = require('../controllers/rua')
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   // Página inicial
   Rua.list()
   .then(data => res.json(Rua.formatParagraphRuas(data)))
+  .catch(erro => res.json(erro))
+})
+*/
+
+router.get('/', function(req, res, next) {
+  // Página inicial
+  var d = new Date().toISOString().substring(0, 16)
+  Rua.list()
+  .then(data => res.render('template',{d:d}))
   .catch(erro => res.json(erro))
 })
 
