f2d68c53cc29735adabcac8e41c1bf02d9e7a745
MiguelCidadeSilva <mikefsi@hotmail.com>
2023-04-07 16:15:19 +0100

    Alterações na parte das entidades.

--- WebSite/routes/entidades.js
+++ WebSite/routes/entidades.js
@@ -5,30 +5,22 @@ var Rua = require('../controllers/rua')
 
 router.get('/', function(req,res,next){
     var d = new Date().toISOString().substring(0, 16)
-    Rua.list()
+    Rua.getNomeEntidades()
     .then(data => {
-      ruas = data
-      entidades = []
-      for(var rua of ruas){
-        const entityNames = rua.entidades.map(entity => entity.nome);
-        entidades = entidades.concat(entityNames);
-    }
-      entidades = Array.from(entidades).map(str => str.charAt(0).toUpperCase() + str.slice(1)).sort();
-      entidades = [...new Set(entidades)];
-      res.render('entidades',{entidades:entidades,ruas:ruas,d:d})
+      res.render('entidades',{entidades:data,ruas:ruas,d:d})
     })
     .catch(erro => res.render('error', {error: erro,d:d}))
 })
 
 
 router.get('/entidade/:entidade', function(req, res, next) {
-    var d = new Date().toISOString().substring(0, 16);
-    var entidade = {}
-    var r = []
-    var f = false
-    entidade["nome"] = req.params.entidade.replaceAll("_"," ")
-    Rua.list()
-      .then(data => {
+  var d = new Date().toISOString().substring(0, 16);
+  var entidade = {}
+  var r = []
+  var f = false
+  entidade["nome"] = req.params.entidade.replaceAll("_"," ")
+  Rua.list()
+    .then(data => {
         var ruas = data;
         for (var rua of ruas) {
           for (var e of rua.entidades) {
@@ -38,18 +30,17 @@ router.get('/entidade/:entidade', function(req, res, next) {
               r.push(rua._id.replace("-"," "))
               f = true
               break;
-              }
             }
           }
+        }
         if (f){
           res.render('entidade', { entidade: entidade, ruas: r, d:d });
         }
         else{
           res.render('entidadeinvalida',{  d:d}); 
         }
-  })
-      .catch(erro => res.render('error', { error: erro,d:d }));
-  });
-
+    })
+    .catch(erro => res.render('error', { error: erro,d:d }));
+});
 
 module.exports = router;
