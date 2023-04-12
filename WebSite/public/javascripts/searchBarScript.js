36e1b5b5e5711a53eaca707b4aeaf27b9700a852
MiguelCidadeSilva <mikefsi@hotmail.com>
2023-04-12 12:26:05 +0100

    Alteração de replace para href de modo a manter o histórico.

--- WebSite/public/javascripts/searchBarScript.js
+++ WebSite/public/javascripts/searchBarScript.js
@@ -1,10 +1,10 @@
 function buscaRua(nomeRua) {
     var rua = nomeRua.replaceAll(' ', '_');
-    window.location.replace('/rua/' + rua);
+    window.location.href='/rua/' + rua;
   }
 
 function buscaEntidade(nomeEntidade) {
   var entidade = nomeEntidade.replaceAll(' ', '_');
-  window.location.replace('/entidades/entidade/' + entidade);
+  window.location.href='/entidades/entidade/' + entidade;
 }
 
