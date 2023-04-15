fecfd4991dabb47fb28315e686afecda861f20f9
Gonçalo <joaogoncalomelo@gmail.com>
2023-04-15 15:34:37 +0100

    datas

--- WebSite/routes/datas.js
+++ WebSite/routes/datas.js
@@ -5,31 +5,46 @@ var Rua = require('../controllers/rua')
 router.get('/', function(req, res, next) {
     var d = new Date().toISOString().substring(0, 16)
     Rua.list()
-    .then(ruas => {
-        var allDatas = []
-        ruas.forEach(rua => {
-            allDatas = allDatas.concat(rua.datas)
+        .then(streets => {
+            var dates = []
+            for (var street of streets){
+                for (var date of street.datas){
+                    dates.push(date)
+                }
+            }
+            res.render('datas', {dates:dates, streets:streets, d:d})
         })
-        sortedAllDatas = [...new Set(allDatas)].sort()
-        res.render('datas',{datas:sortedAllDatas, ruas:ruas, d:d})
-    })
-    .catch(erro => res.render('error', {error: erro}))
+        .catch(erro => {
+            res.render('error', {error:erro})
+        })
 });
 
-
-
-router.get('/', function(req, res, next) {
+router.get('/data/:data', function(req, res, next) {
     var d = new Date().toISOString().substring(0, 16)
-    Rua.listAllDatas()
-    .then(data => {
-        res.render('datas',{datas:data, d:d})
-    })
-    .catch(erro => res.render('error', {error: erro}))
+    var dateRes = req.params.data.replaceAll('_', ' ')
+    var streetsRes = []
+    var flag = false
+    Rua.list()
+        .then(streets => {
+            for (var street of streets){
+                for (var date of street.datas){
+                    if (date === dateRes){
+                        streetsRes.push(street._id.replace(" ", "_"))
+                        flag = true
+                        break
+                    }
+                }
+            }
+            if (flag){
+                res.render('data', {date:dateRes, streets:streetsRes, d:d});
+            }
+            else{
+                res.render('dataInvalida',{d:d}); 
+            }
+        })
+        .catch(erro => {
+            res.render('error', {error:erro, d:d});
+        })
 });
-
-
-router.get('/data/:data', function(req,res,next){
-
-})  
 
 module.exports = router;
